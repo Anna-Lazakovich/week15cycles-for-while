a1d0afc4bfdfac9eab604dc2e99e9a23ab98d42c
Anna-Lazakovich <anna27659@gmail.com>
2023-12-02 20:13:21 +0300

    task2

---
+++ task2/week15.js
@@ -0,0 +1,24 @@
+const cities = ['Минск', 'Стамбул', 'Батуми', 'Токио'];
+let temps = [];
+
+const list = document.getElementById('list');
+const max = document.getElementById('max');
+const min = document.getElementById('min');
+
+for (let i = 0; i < cities.length; i++) {
+temps.push(prompt(`Введите температуру для города ${cities[i]}`, ''));
+};
+
+for (let j = 0; j < temps.length; j++) {
+const newCity = document.createElement('li');
+newCity.textContent = `Температура в городе ${cities[j]} ${temps[j]} °C`;
+list.appendChild(newCity); 
+};
+
+
+(function findMinMax() {
+    let maxValue = Math.max(...temps);
+    let minValue = Math.min(...temps); 
+    max.textContent = `Максимальная температура: ${maxValue} °C`;
+    min.textContent = `Минимальная температура: ${minValue} °C`;
+})();
